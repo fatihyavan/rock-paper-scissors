cf82511e8d1d6b6110c6a152b1321da9a1e4fe80
fatihyavan <fatih.yavan@outlook.com>
2022-11-27 21:32:00 +0300

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Home from './apps/Home';
 function App() {
   return (
     <div>
-      <div>Navbar</div>
       <Home />
     </div>
   );
--- client/src/apps/Home.js
+++ client/src/apps/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import {Newspaper} from 'phosphor-react'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className='text-3xl font-bold  bg-red-900'>Home
+    <div className='text-3xl font-bold  bg-red-900'>
+      <Navbar />
+      Home
       <div className='flex bg-blue-200'>
         <Newspaper size={32} />
       </div>
