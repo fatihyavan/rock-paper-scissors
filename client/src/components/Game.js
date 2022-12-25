8ae07850dba7e313aa3868b95d84c5db212662fc
fatihyavan <fatih.yavan@outlook.com>
2022-12-25 21:22:30 +0300

    first mode finish

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -5,27 +5,85 @@ import scissors from '../images/scissors.png'
 
 export default function Game() {
   const [choice,setChoice] = useState("");
-  
-  const makeChoice = (e)=>{
-    console.log(e.target.value);
-    console.log(e.target.dataset.id);
-    console.log("bbb");
-   setChoice(e.target.dataset.id);
-  };
+  const [robotChoice,setRobotChoice] = useState("");
+  const [result,setResult] = useState("");
 
-useEffect(()=>{
-  console.log(choice);
-},[choice]);
+  const choiceList = [
+    'rock','paper','scissors'
+  ];
+  
+  useEffect(()=>{
+    calculateWin();
+  },[choice,robotChoice]);
+
+
+  const makeChoice = (e)=>{
+     setChoice(e.target.getAttribute('data-id'));
+     makeRobotChoice();
+    
+    
+  };
+  const makeRobotChoice = ()=>{
+    setRobotChoice(choiceList[Math.floor(Math.random()*choiceList.length)]);
+  }
+
+  function calculateWin(){
+  if(choice==="rock"){
+    switch(robotChoice){
+      case 'paper':
+         setResult("loss")
+        break
+      case 'scissors':
+         setResult('win')
+        break
+      default:
+         setResult('draw')
+        break
+    }
+  }
+  else if (choice === 'paper'){
+    switch(robotChoice){
+      case 'scissors':
+         setResult('loss')
+        break
+      case 'rock':
+         setResult('win')
+        break
+      default:
+         setResult('draw')
+        break
+    }
+  }
+  else if(choice === 'scissors'){
+    switch(robotChoice){
+      case 'rock':
+         setResult('loss')
+        break
+      case 'paper':
+         setResult('win')
+        break
+      default:
+         setResult('draw')
+        break
+    }
+  }
+  }
 
   return (
     <div>
       <div className='flex justify-center'> Taş,kağıt,makastan birini seçiniz</div>
       <div className='flex justify-center space-x-20 '>
-        <div data-id="rock" onClick={makeChoice} ><img src={rock} alt={rock}></img></div>
-        <div data-id="paper" onClick={makeChoice} ><img src={paper} alt={paper}></img></div>
-        <div data-id="scissor" onClick={makeChoice} ><img src={scissors} alt={scissors}></img></div>
+        <div className='pt-10 rounded-full border-2 bg-blue-500'><img src={rock} alt={rock} data-id="rock" onClick={makeChoice} /></div>
+        <div><img src={paper} alt={paper} data-id="paper" onClick={makeChoice} /></div>
+        <div className='pt-10'><img src={scissors} alt={scissors}  data-id="scissors" onClick={makeChoice} /></div>
         
       </div>
+      <div>Sonuc
+        <div>{result}</div>
+        <div>Sizin seçimizi:{choice}</div>
+        <div>Robotun seçimi:{robotChoice}</div>
+      </div>
+      
     </div>
      
   )
--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const scoreboard = require("./src/config/scoreboard");
+const scoreboard = require("./src/router/scoreRouter");
+const sequelize = require("./src/config/database");
 
 
-app.use()
+app.use("/score",scoreboard);
+
+sequelize.sync()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server has alive ${process.env.PORT}`);
